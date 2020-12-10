37a1a2bf3a660fcfcceadb48f94c5cc18085a2f9
AndreasE <63844978+AndreasExner@users.noreply.github.com>
2020-12-10 18:23:52 +0100

    Update LastUpdateTimeString.js

--- LastUpdateTimeString.js
+++ LastUpdateTimeString.js
@@ -1,5 +1,5 @@
 on({id: "0_userdata.0.IoT-Devices.04.SensorIP", change: "any"}, function (obj) {
   var value = obj.state.val;
   var oldValue = obj.oldState.val;
-  setState("0_userdata.0.IoT.IndoorAirSensor02.LastUpdate", ([formatDate(new Date(), "DD.MM."),' - ',formatDate(new Date(), "hh:mm")].join('')), true);
+  setState("0_userdata.0.IoT.IndoorAirSensor.LastUpdate", ([formatDate(new Date(), "DD.MM."),' - ',formatDate(new Date(), "hh:mm")].join('')), true);
 });
